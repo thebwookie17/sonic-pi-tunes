1b0f65dc639700cc961a89e68b909612678ac27f
Meagan Bartholomew <meaganbartholomew@Meagans-MBP.lan>
2021-04-23 17:43:20 -0500

    La Vie En Rose Bars 1-3

--- la_vie_en_rose.rb
+++ la_vie_en_rose.rb
@@ -1,12 +1,32 @@
-# Sources
-# Audio Samples:
-# https://www.youtube.com/watch?v=eu1auaUHy0c
-# https://www.youtube.com/watch?v=jcKypsr-VL4
-# Tools:
-# Transposing tool
-# Sheet Musice samples:
-# https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0190978
-# https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0193822&ca=0&cmpid=art_the_piano_keys&utm_source=art_the_piano_keys&utm_medium=campaign
+# ---------------------------------------- ABOUT ---------------------------------------- #
+#
+# Transcribed to Sonic-Pi Ruby Code for Stanton Bartholomew by wife, Meagan Bartholomew.
+# With all my love; Happy anniversary. <3
+#
+# --------------------------------------------------------------------------------------- #
+
+# --------------------------------------- CREDITS --------------------------------------- #
+#
+# La Vie En Rose (Take Me To Your Heart Again) for Cello and Piano
+#   Music by Luis Guglielmi
+#   Original French Lyrics by Edith Piaf
+#   English Lyrics by David Mack
+#   Arranged by Brooklyn Duo
+#   Source: https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0190978
+#   Bar 1 transposed from Ab Major to C Major for this arrangement.
+#
+# La Vie En Rose ( Take Me To Your Heart)
+#   Music by Luis Guglielmi
+#   Original French Lyrics by Edith Piaf
+#   English Lyrics by David Mack
+#   Arranged by The Piano Keys
+#   Source: https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0193822&ca=0&cmpid=art_the_piano_keys&utm_source=art_the_piano_keys&utm_medium=campaign
+#   Source: https://www.youtube.com/watch?v=jcKypsr-VL4
+#   Primary source for this arrangement
+#
+# --------------------------------------------------------------------------------------- #
+
+# --------------------------------------- FUNCTIONS --------------------------------------- #
 
 define :play_piano_note do | note, length |
   play note, decay: length*0.9, release: length*0.1, amp: 1
@@ -24,23 +44,53 @@ define :arpeggio do |notes, length|
   sleep length
 end
 
+# ----------------------------------------------------------------------------------------- #
+
+# --------------------------------------- MAIN --------------------------------------- #
+
 with_fx :reverb,room: 0.5 do
   use_synth :piano
   use_bpm 64
   
   in_thread(name: :main) do
-    ##| Ab | Bb | Db | Eb
-    arpeggio [:Ab3, :C4, :Eb4], 0.5
-    play_piano_note [:D4, :F4], 0.5
-    play_piano_note [:F4, :Ab4], 0.5
-    play_piano_note [:Bb4], 0.5
+    ##| 1
+    arpeggio [:C4, :E4, :G4], 0.5
+    play_piano_note [:F4, :A4], 0.5
+    play_piano_note [:A4, :C5], 0.5
+    play_piano_note [:D5], 0.5
+    in_thread do
+      arpeggio [:C4, :C5, :G5], 2
+    end
+    in_thread do
+      sleep 1
+      play_piano_note [:G4, :G5], 1
+    end
+    sleep 2
     
-    arpeggio [:Gs4, :Ab4, :Eb5], 2
+    use_bpm 70
+    
+    ##| 3
+    arpeggio [:F4, :A4, :D5], 0.5
+    play_piano_note [:D5], 1
+    play_piano_note [:C5], 0.5
+    arpeggio [:F4, :Ab4, :D5], 0.5
+    play_piano_note [:D5], 1
+    play_piano_note [:D5], 0.5
+    
   end
   
   in_thread(name: :suport) do
-    ##| Ab | Bb | Db | Eb
-    arpeggio [:Bb3], 2
-    arpeggio [:Eb3,:Db4,:F4], 2
+    #4/4 Time
+    
+    ##| 1
+    arpeggio [:D3], 2
+    arpeggio [:G3,:F4,:A4], 2
+    
+    ##| 3
+    arpeggio [:D3,:C4], 2
+    arpeggio [:D3,:B3], 2
   end
 end
+
+# ------------------------------------------------------------------------------------ #
+
