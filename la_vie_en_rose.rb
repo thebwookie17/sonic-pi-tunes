b3e7a92962d7509befe7454757428b7ca91f658a
thebwookie17 <38009472+thebwookie17@users.noreply.github.com>
2021-10-05 21:07:36 -0500

    Update la_vie_en_rose.rb

--- la_vie_en_rose.rb
+++ la_vie_en_rose.rb
@@ -28,6 +28,10 @@
 
 # --------------------------------------- FUNCTIONS --------------------------------------- #
 
+define :rest do |length|
+  sleep length
+end
+
 define :play_piano_note do | note, length |
   play note, decay: length*0.9, release: length*0.1, amp: 1
   sleep length
@@ -94,6 +98,7 @@ with_fx :reverb,room: 0.5 do
     play_piano_note [:D5], 0.5
     
     ##| 4
+
     
     ##| 5
     
@@ -260,64 +265,222 @@ with_fx :reverb,room: 0.5 do
     play_piano_note [:Gb2], 1
     
     ##| 15
+    play_piano_note [:F2], 1
+    play_piano_note [:D3], 0.125
+    play_piano_note [:F3], 0.125
+    play_piano_note [:A3], 0.125
+    play_piano_note [:C4], 0.125
+    ##| TREBBLE
+    play_piano_note [:D4], 0.125
+    play_piano_note [:F4], 0.125
+    play_piano_note [:A4], 0.125
+    play_piano_note [:C5], 0.125
+    play_piano_note [:G5], 2
     
     ##| 16
+    ##| BASE
+    play_piano_note [:F2], 1
+    play_piano_note [:A3, :C4], 1
+    play_piano_note [:F2], 1
+    play_piano_note [:Ab3, :C4], 1
     
     ##| 17
+    play_piano_note [:E2], 1
+    play_piano_note [:G3, :C4], 1
+    play_piano_note [:C2], 1
+    play_piano_note [:E3, :Bb3, :D4], 1
     
     ##| 18
+    play_piano_note [:F2], 1
+    play_piano_note [:A3, :C4], 1
+    play_piano_note [:F2], 1
+    play_piano_note [:Ab3, :C4], 1
     
     ##| 19
+    play_piano_note [:D2], 1
+    play_piano_note [:E3, :Fs3, :C4], 1
+    play_piano_note [:G2], 1
+    play_piano_note [:F3, :B3, :D4], 1
     
-    ##| 20
-    
-    ##| 21
+    ##| 20 - 21
+    2.times do
+      play_piano_note [:C3], 1
+      play_piano_note [:G3, :B3], 1
+      play_piano_note [:G2], 1
+      play_piano_note [:G3, :B3], 1
+    end
     
     ##| 22
+    play_piano_note [:D3], 1
+    play_piano_note [:A3, :C4], 1
+    play_piano_note [:G2, :F3], 2
     
     ##| 23
+    play_piano_note [:C2], 1
+    play_piano_note [:G3, :B3], 1
+    play_piano_note [:Bb2, :Ab3], 1
+    play_piano_note [:Eb2], 1
     
-    ##| 24
+    ##| KEY CHANGE: Ab Bb Db Eb
     
-    ##| 25
+    ##| 24 - 25
+    2.times do
+      play_piano_note [:Ab1], 1
+      play_piano_note [:Eb3, :Ab3, :C4], 1
+      play_piano_note [:Eb2], 1
+      play_piano_note [:Eb3, :Ab3, :C4], 1
+    end
     
     ##| 26
+    play_piano_note [:Ab1], 1
+    play_piano_note [:Eb3, :Ab3, :C4], 1
+    play_piano_note [:Ab2], 1
+    play_piano_note [:F3, :B3, :D4], 1
     
     ##| 27
+    play_piano_note [:F3, :Ab3, :Bb3, :Db4], 1
+    play_piano_note [:Bb2], 1
+    play_piano_note [:Eb2], 1
+    play_piano_note [:Eb3, :G3, :Db4], 1
     
     ##| 28
+    play_piano_note [:Bb1], 1
+    play_piano_note [:Bb3, :Db4, :F4], 1
+    play_piano_note [:Eb2], 1
+    play_piano_note [:Bb3, :Db4, :F4], 1
     
     ##| 29
+    play_piano_note [:Bb1], 1
+    play_piano_note [:Eb3, :G3, :Db3], 1
+    play_piano_note [:Eb2], 1
+    play_piano_note [:Eb3, :G3, :Db3], 1
     
     ##| 30
+    play_piano_note [:Bb1], 0.5
+    play_piano_note [:F3, :Db4], 0.5
+    play_piano_note [:G3, :Eb4], 0.5
+    play_piano_note [:Ab3, :Db4, :F4], 0.5
+    play_piano_note [:G3, :Db4, :F4], 1
+    play_piano_note [:Eb2], 1
     
     ##| 31
+    play_piano_note [:Ab2], 1
+    play_piano_note [:Bb3, :C4, :F4], 1
+    play_piano_note [:Eb2], 1
+    play_piano_note [:Eb3, :G3, :Db4], 1
     
     ##| 32
+    play_piano_note [:Ab1, :Ab2], 1
+    play_piano_note [:Ab3, :C4, :Eb4], 1
+    play_piano_note [:Eb2, :Eb3], 1
+    play_piano_note [:Ab1, :C4, :Eb4], 1
     
     ##| 33
+    play_piano_note [:Ab1, :Ab2], 1
+    play_piano_note [:G3, :C4, :Eb4], 1
+    play_piano_note [:Eb2, :Eb3], 1
+    play_piano_note [:Ab1, :C4, :Eb4], 1
     
     ##| 34
+    play_piano_note [:Ab1, :Ab2], 1
+    play_piano_note [:Eb3, :Bb3, :C4], 1
+    play_piano_note [:Eb2, :Eb3], 1
+    play_piano_note [:D2, :D3], 1
     
     ##| 35
+    in_thread do
+      play_piano_note [:Db2, :Db3], 4
+    end
+    in_thread do
+      rest 0.6875
+      play_piano_note [:Db3], 0.0625
+      play_piano_note [:F3], 0.0625
+      play_piano_note [:Ab3], 0.0625
+      play_piano_note [:C4], 0.0625
+      play_piano_note [:Eb4], 0.0625
+      play_piano_note [:F4], 0.0625
+      play_piano_note [:Ab4], 0.0625
+      ##| TREBBLE
+      play_piano_note [:Db4], 0.0625
+      play_piano_note [:F4], 0.0625
+      play_piano_note [:Ab4], 0.0625
+      play_piano_note [:C5], 0.0625
+      play_piano_note [:Eb5], 0.0625
+      play_piano_note [:F5], 0.0625
+      play_piano_note [:Ab5], 0.0625
+      play_piano_note [:Db5], 0.0625
+      play_piano_note [:F5], 0.0625
+      play_piano_note [:Ab5], 0.0625
+      play_piano_note [:C5], 0.0625
+      play_piano_note [:Eb5], 0.0625
+      play_piano_note [:F5], 0.0625
+      play_piano_note [:Ab5], 0.0625
+      play_piano_note [:Db2], 2
+    end
+    sleep 4
+    
+    ##| TREBBLE
     
     ##| 36
+    play_piano_note [:Bb3, :F4, :Ab4], 2
+    play_piano_note [:Bb3, :E4, :Ab4], 2
     
     ##| 37
+    play_piano_note [:Bb3, :Eb4, :G4], 2
+    play_piano_note [:Ab3, :Eb4, :G4], 2
+
+    ##| BASE
     
     ##| 38
+    play_piano_note [:Db3], 1
+    play_piano_note [:Ab3, :Db4, :F4], 1
+    play_piano_note [:Db3], 1
+    play_piano_note [:Ab3, :D4, :Eb4], 1
     
     ##| 39
+    play_piano_note [:Db3], 1
+    play_piano_note [:Bb3, :F4, :Ab4], 1
+    play_piano_note [:Eb3], 1
+    play_piano_note [Bb3:, :Eb4, :G4], 1
+
+    ##| TREBBLE
     
     ##| 40
+    2.times do
+      play_piano_note [:Ab3], 0.5
+      play_piano_note [:Eb4], 0.5
+      play_piano_note [:G4], 1
+    end
+
+    ##| BASE
     
     ##| 41
+    2.times do
+      play_piano_note [:Eb3], 0.5
+      play_piano_note [:Ab3], 0.5
+      play_piano_note [:Eb4], 1
+    end
     
     ##| 42
+    play_piano_note [:Eb3], 0.5
+    play_piano_note [:Bb4], 0.5
+    play_piano_note [:F4], 1
+    play_piano_note [:Eb3], 0.5
+    play_piano_note [:Bb4], 0.5
+    play_piano_note [:G4], 1
+
+    ##| TREBBLE
     
-    ##| 43
-    
-    ##| 44
+    ##| 43 - 44
+    in_thread do
+      play_piano_note [:Ab3], 8
+    end
+    in_thread do
+      rest 0.5
+      play_piano_note [:Eb4], 0.5
+      play_piano_note [:C5], 7
+    end
+    sleep 8
   end
 end
 
